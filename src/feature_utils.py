0b62c95c43473fa887799db10071cd3e1e948acc
IntroML2026 <f.l.sohnoume@tcu.edu>
2026-02-26 13:28:16 -0600

    Add files via upload

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -20,8 +20,8 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['COST', 'WMT', 'TGT'] #['MSFT', 'IBM', 'GOOGL']
-    ccy_tickers = ['DEXCAUS', 'DEXJPUS']
+    stk_tickers = ['MSFT', 'IBM', 'GOOGL']
+    ccy_tickers = ['DEXJPUS', 'DEXUSUK']
     idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
@@ -29,53 +29,20 @@ def extract_features():
     ccy_data = web.DataReader(ccy_tickers, 'fred', start=START_DATE, end=END_DATE)
     idx_data = web.DataReader(idx_tickers, 'fred', start=START_DATE, end=END_DATE)
 
-    return_period = 5
+    Y = np.log(stk_data.loc[:, ('Adj Close', 'MSFT')]).diff(return_period).shift(-return_period)
+    Y.name = Y.name[-1]+'_Future'
     
-    # ===== Target (5-day future log return on COST) =====
-    Y = np.log(stk_data.loc[:, ('Adj Close', 'COST')]).diff(return_period).shift(-return_period)
-    Y.name = 'COST_Future'
-    
-    
-    # ===== Core Features =====
-    # Correlated stocks (WMT, TGT)
-    X1 = np.log(stk_data.loc[:, ('Adj Close', ['WMT', 'TGT'])]).diff(return_period)
-    X1.columns = X1.columns.droplevel(0)  # drops "Adj Close" level -> ['WMT','TGT']
-    
-    # FX + indices
+    X1 = np.log(stk_data.loc[:, ('Adj Close', ('GOOGL', 'IBM'))]).diff(return_period)
+    X1.columns = X1.columns.droplevel()
     X2 = np.log(ccy_data).diff(return_period)
     X3 = np.log(idx_data).diff(return_period)
-    
-    
-    # ===== NEW Technical Features on COST =====
-    open_ = stk_data.loc[:, ('Open', 'COST')]
-    high_ = stk_data.loc[:, ('High', 'COST')]
-    low_  = stk_data.loc[:, ('Low', 'COST')]
-    close_ = stk_data.loc[:, ('Close', 'COST')]
-    
-    # 1) Range
-    range_feat = ((high_ - low_) / open_).to_frame('Range')
-    
-    # 2) Gap
-    gap_feat = ((open_ - close_.shift(1)) / close_.shift(1)).to_frame('Gap')
-    
-    # 3) Intraday return
-    intraday_feat = ((close_ - open_) / open_).to_frame('Intraday_Return')
-    
-    # 4) Rolling volatility based on return_period log returns
-    cost_r = np.log(stk_data.loc[:, ('Adj Close', 'COST')]).diff(return_period)
-    roll_vol_feat = cost_r.rolling(10).std().to_frame('Rolling_Vol_10')
-    
-    X4 = pd.concat([range_feat, gap_feat, intraday_feat, roll_vol_feat], axis=1)
-    
-    
-    # ===== Final dataset =====
-    X = pd.concat([X1, X2, X3, X4], axis=1)
+
+    X = pd.concat([X1, X2, X3], axis=1)
     
     dataset = pd.concat([Y, X], axis=1).dropna().iloc[::return_period, :]
-    Y = dataset[Y.name]
-    X = dataset[X.columns]
+    Y = dataset.loc[:, Y.name]
+    X = dataset.loc[:, X.columns]
     dataset.index.name = 'Date'
-
     #dataset.to_csv(r"./test_data.csv")
     features = dataset.sort_index()
     features = features.reset_index(drop=True)
@@ -99,7 +66,4 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
-
-
-
 
