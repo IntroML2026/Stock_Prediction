7a30cfb5fac6e034da583a288f9875a926bc82c0
IntroML2026 <f.l.sohnoume@tcu.edu>
2026-02-26 14:56:44 -0600

    Add files via upload

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -27,8 +27,7 @@ warnings.simplefilter("ignore")
 # Fix path for Streamlit Cloud (ensure 'src' is findable)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
-import src.feature_utils
-importlib.reload(src.feature_utils)
+
 if project_root not in sys.path:
     sys.path.append(project_root)
 
