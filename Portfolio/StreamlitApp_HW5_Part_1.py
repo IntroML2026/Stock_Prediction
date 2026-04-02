42ae7d18bbcc2f8520a01cb1b7cdf6a07db4317d
IntroML2026 <f.l.sohnoume@tcu.edu>
2026-04-02 15:17:08 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -116,12 +116,12 @@ def display_explanation(input_df, session, aws_bucket):
     explainer_name = MODEL_INFO["explainer"]
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
     
-    #best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
-    #preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-2])
-    #input_df_transformed = preprocessing_pipeline.transform(input_df)
-    #feature_names = best_pipeline[1:4].get_feature_names_out()
-    #input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
-    shap_values = explainer(input_df)
+    best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
+    preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[0:1])
+    input_df_transformed = preprocessing_pipeline.transform(input_df)
+    feature_names = best_pipeline[0:1].get_feature_names_out()
+    input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
+    shap_values = explainer(input_df_transformed)
     
     st.subheader("🔍 Decision Transparency (SHAP)")
     fig, ax = plt.subplots(figsize=(10, 4))
