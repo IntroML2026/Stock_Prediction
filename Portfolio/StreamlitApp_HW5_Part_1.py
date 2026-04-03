7a9841cb463c03a88cda452ddfa5abe375dc9e72
IntroML2026 <f.l.sohnoume@tcu.edu>
2026-04-02 19:44:45 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -119,8 +119,8 @@ def display_explanation(input_df, session, aws_bucket):
     explainer = load_shap_explainer(session, aws_bucket, posixpath.join('explainer', explainer_name),os.path.join(tempfile.gettempdir(), explainer_name))
 
     raw_json_input = json.dumps(input_df)
-    clean_df = inference_pca.input_fn(raw_json_input, 'application/json')
-    st.text(type(clean_df))
+    #clean_df = inference_pca.input_fn(raw_json_input, 'application/json')
+    #st.text(type(clean_df))
 
     dataset = pd.read_csv(r'./SP500Data.csv',index_col=0)
     random = 'IBM'
@@ -133,7 +133,7 @@ def display_explanation(input_df, session, aws_bucket):
     X = np.exp(X).cumsum()
     X.columns = [name + "_CR_Cum" for name in X.columns]
 
-    input_df = clean_df #X.loc[[closest_date]]
+    input_df = X.loc[[closest_date]] #clean_df #
 
     best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
     
