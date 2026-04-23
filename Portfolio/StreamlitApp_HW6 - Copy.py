997da78a7d0f7d5d20a95a69604080b15cf6a240
IntroML2026 <f.l.sohnoume@tcu.edu>
2026-04-22 21:29:13 -0500

    Update StreamlitApp_HW6 - Copy.py

--- Portfolio/StreamlitApp_HW6 - Copy.py
+++ Portfolio/StreamlitApp_HW6 - Copy.py
@@ -146,6 +146,7 @@ def display_explanation(input_df, session, aws_bucket):
     
     best_pipeline = load_pipeline(session, aws_bucket, 'sklearn-pipeline-deployment')
     preprocessing_pipeline = Pipeline(steps=best_pipeline.steps[:-3])
+    input_df=pd.DataFrame(input_df)
     input_df_transformed = preprocessing_pipeline.transform(input_df)
     feature_names = best_pipeline[:-3].get_feature_names_out()
     input_df_transformed = pd.DataFrame(input_df_transformed, columns=feature_names)
